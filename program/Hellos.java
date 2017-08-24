8a09382d5c9c3b8331de7ff35abf68cfcacaaf81
Nick Plucker <pluck011@morris.umn.edu>
2017-08-24 15:42:50 -0500

    Added a method for saying hello.

--- program/Hellos.java
+++ program/Hellos.java
@@ -6,20 +6,13 @@ public class Hellos {
 
     public static void main(String[] args) {
         StringBuilder builder = new StringBuilder();
-
-        builder.append("Hello, folks!\n");
-        builder.append(nic_says_hello());
-        builder.append(kk_says_hello());
-
+        builder.append(Danish_and_Nick());
         System.out.println(builder.toString());
+
+
     }
 
-    private static String nic_says_hello() {
-        return "Nic says 'Howdy!'\n";
+    private static String Danish_and_Nick() {
+        return "Danish and Nick say 'Hello!'\n";
     }
-
-    private static String kk_says_hello() {
-        return "KK says 'Hello!'\n";
-    }
-
 }
